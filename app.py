ee1f667f9a53592fa9448410fadc6403053327ab
7rensenS <rensenselvan1778@gmail.com>
2019-02-27 14:24:34 +0530

    webpage

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask  
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>hello you are in...</h1>"\
+    return render_template("index.html")
 
 @app.route("/home")
 def home():
@@ -12,11 +12,15 @@ def home():
 
 @app.route("/about")
 def about():
-    return "you are in about"
+    return render_template("about.html")
 
 @app.route("/contact")
 def contact():
-    return "you are in contact"
+   return render_template("contact.html")
+
+@app.route("/webpage")
+def webpg():
+   return render_template("webpg.html")
 
 if(__name__=="__main__"):
     app.run(debug=True)
